f12bfc0b3d33e263909a3e9a8570f074ff75d642
louanbastos <louanbastos61@gmail.com>
2018-11-20 18:43:58 -0300

    fix

--- database/migrations/2018_11_20_214335_create_products_table.php
+++ database/migrations/2018_11_20_214335_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProdutosTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateProdutosTable extends Migration
      */
     public function up()
     {
-        Schema::create('produtos', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('code', 50);
-            $table->string('description', 200);
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateProdutosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('produtos');
+        Schema::dropIfExists('products');
     }
 }
